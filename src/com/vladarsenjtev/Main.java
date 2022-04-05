9e20e4862ba7ff24f71e4534ab7eff909fa1c8dc
Vlad Arsenjtev <Vlad.arsenjtev@gmail.com>
2022-04-05 22:49:40 +0300

    Task_3

--- src/com/vladarsenjtev/Main.java
+++ src/com/vladarsenjtev/Main.java
@@ -3,6 +3,10 @@ package com.vladarsenjtev;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println(stringReverse("Apollo"));
+    }
 
+    public static String stringReverse(String str) {
+        return new StringBuilder(str).reverse().toString();
     }
 }
