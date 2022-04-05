030f8f88f0daa6c502fbcce57b042ba13a25d74e
Vlad Arsenjtev <Vlad.arsenjtev@gmail.com>
2022-04-05 22:54:20 +0300

    Task_4

--- src/com/vladarsenjtev/Main.java
+++ src/com/vladarsenjtev/Main.java
@@ -3,6 +3,15 @@ package com.vladarsenjtev;
 public class Main {
 
     public static void main(String[] args) {
+        String rose = "А роза упала на лапу Азора";
+        System.out.println(rose);
+        System.out.println(isPalindrome(rose));
+    }
 
+    public static boolean isPalindrome(String str) {
+        str = str.replaceAll("\\s+", "");
+        String reverseRose = new StringBuilder(str).reverse().toString();
+        return str.equalsIgnoreCase(reverseRose);
     }
 }
+
